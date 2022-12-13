4fe7c2fbcf90b9252820a4aaf9b7c587e478af59
foozbat <github@foozbat.net>
2022-12-13 14:15:36 +0000

    formatting updates

--- src/Model.php
+++ src/Model.php
@@ -40,15 +40,13 @@ abstract class Model implements Iterator
     public function __construct(mixed ...$params)
     {
         // if a primary key is passed to constructor, set primary key member
-        if (isset($params[self::__primary_key__]))
-        {
+        if (isset($params[self::__primary_key__])) {
             $this->{$this::__primary_key__} = $params[self::__primary_key__];
             $this->load();
         }
         
         // if any model params were passed to constructor, set them
-        if (sizeof($params) > 0) 
-        {
+        if (sizeof($params) > 0) {
             $this->set_model_data($params);
         }
     }
@@ -81,8 +79,7 @@ abstract class Model implements Iterator
         $cls = get_called_class();
         $table = $cls::__table__;
 
-        if ($table == '')
-        {
+        if ($table == '') {
             if ($pos = strrpos($cls, '\\')) {
                 $cls = substr($cls, $pos + 1);
             }
@@ -103,8 +100,7 @@ abstract class Model implements Iterator
 
         $arr = get_object_vars($this);
 
-        foreach ($arr as $var => $val)
-        {
+        foreach ($arr as $var => $val) {
             if ((str_starts_with($var, "__") && str_ends_with($var, "__")) ||
                 !in_array($var, $table_columns)) {
                 unset($arr[$var]);
@@ -124,8 +120,7 @@ abstract class Model implements Iterator
     {
         $arr = $this->get_model_vars();
 
-        foreach ($arr as $var => $val)
-        {
+        foreach ($arr as $var => $val) {
             if (property_exists(get_class($this), $var) && isset($data[$var]) && !str_starts_with($var, "__") && !str_ends_with($var, "__")) {
                 $this->{$var} = $data[$var];
             }
@@ -166,10 +161,12 @@ abstract class Model implements Iterator
 
         $data = $this->db()->selectrow_assoc($query, $this::__primary_key__);
 
-        if ($data === false)
+        if ($data === false) {
             return false;
-        else
+        } else {
             $this->set_model_data($data);
+        }
+
         return true;
     }
 
@@ -187,15 +184,15 @@ abstract class Model implements Iterator
         $cls::db()->prepare("SELECT * FROM $table");
         $cls::db()->execute();
 
-        while ($row = $cls::db()->fetchrow_assoc())
-        {
+        while ($row = $cls::db()->fetchrow_assoc()) {
             array_push($ret_arr, new $cls(...$row));
         }
 
-        if (sizeof($ret_arr) == 0)
+        if (sizeof($ret_arr) == 0) {
             return null;
-        else
-            return sizeof($ret_arr) == 1 ? $ret_arr[0] : $ret_arr;   
+        } else {
+            return sizeof($ret_arr) == 1 ? $ret_arr[0] : $ret_arr;
+        }
     }
 
     /**
@@ -212,14 +209,12 @@ abstract class Model implements Iterator
 
         $query = "SELECT * FROM $table";
         
-        if (sizeof($params) > 0)
-        {
+        if (sizeof($params) > 0) {
             $table_columns = $cls::db()->get_column_names($table);
             $query_fields = array();
             $query_values = array();
 
-            foreach ($params as $field => $value)
-            {
+            foreach ($params as $field => $value) {
                 if (!in_array($field, $table_columns)) {
                     throw new ModelException("Table column '$field' does not exist.");
                 }
@@ -227,28 +222,53 @@ abstract class Model implements Iterator
                 array_push($query_values, $value);
             }
 
-            if (sizeof($query_values) > 0)
+            if (sizeof($query_values) > 0) {
                 $query .= " WHERE ".implode(" AND ", $query_fields);
+            }
         }
 
         $cls::db()->prepare($query);
         $cls::db()->execute(...$query_values);
 
-        while ($row = $cls::db()->fetchrow_assoc())
-        {
+        while ($row = $cls::db()->fetchrow_assoc()) {
             array_push($ret_arr, new $cls(...$row));
         }
 
-        if (sizeof($ret_arr) == 0)
+        if (sizeof($ret_arr) == 0) {
             return null;
-        else
-            return sizeof($ret_arr) == 1 ? $ret_arr[0] : $ret_arr;      
+        } else {
+            return sizeof($ret_arr) == 1 ? $ret_arr[0] : $ret_arr;
+        }
     }
 
-    // iterator methods
-    public function current(): mixed { return $this; }
-    public function next(): void     { $this->__iter__++; }
-    public function valid(): bool    { return $this->__iter__ == 0; }
-    public function key(): mixed     { return $this->__iter__; }
-    public function rewind(): void   { $this->__iter__ = 0; }
+    /**
+     * Iterator Methods
+     *   Allows the developer to iterate over a single model object or array of objects
+     *   via foreach if desired.
+     */
+
+    public function current(): mixed
+    {
+        return $this;
+    }
+
+    public function next(): void
+    {
+        $this->__iter__++;
+    }
+
+    public function valid(): bool
+    {
+        return $this->__iter__ == 0;
+    }
+
+    public function key(): mixed
+    {
+        return $this->__iter__;
+    }
+
+    public function rewind(): void
+    {
+        $this->__iter__ = 0;
+    }
 }
