6d94b1b4e5c1957755e093901552aa74ba995d1c
foozbat <github@foozbat.net>
2024-03-04 19:33:38 +0000

    add session invalidation, fixes

--- src/Auth.php
+++ src/Auth.php
@@ -47,14 +47,15 @@ class Auth
         $this->auth_token_name = (defined('APP_NAME') ? APP_NAME : 'fzb_app') . '_auth_token';
         $this->csrf_token_name = (defined('APP_NAME') ? APP_NAME : 'fzb_app') . '_csrf_token';
 
+        // try to get session from auth token cookie
         if (isset($_COOKIE[ $this->auth_token_name ])) {
-            $this->user_session = $user_session_cls::get_by(auth_token: $_COOKIE[ $this->auth_token_name ]);
+            $this->user_session = $user_session_cls::get_by(auth_token: $_COOKIE[ $this->auth_token_name ], logged_in: true);
         }
         
         if ($this->user_session !== null) {
             $this->user = $user_cls::get_by(id: $this->user_session->user_id);
 
-            if ($this->user !== null && $this->user_session->logged_in && $this->user_session->validate_fingerprint()) {
+            if ($this->user !== null && $this->user_session->validate_fingerprint()) {
                 $this->is_authenticated = true;
             }
 
@@ -68,7 +69,7 @@ class Auth
 
     
     /**
-     * interface for retrieving the Config singleton
+     * interface for retrieving the Auth singleton
      *
      * @return Auth Auth instance
      */
@@ -81,13 +82,23 @@ class Auth
         return self::$instance;
     }
 
+    /**
+     * Attempts to log in a user via username/password.  
+     * If authenticated, creates a new user session and sets the session token cookie.
+     *
+     * @param string $username username
+     * @param string $password password
+     * @return boolean login success/failure
+     */
     public function login(string $username, string $password): bool
     {
         if (!$username || !$password) {
             return false;
         }
 
-        $user = User::get_by(username: $username);
+        $user = $this->user_cls::get_by(username: $username);
+
+        //var_dump($user);
 
         if ($user === null) {
             return false;
@@ -95,7 +106,16 @@ class Auth
 
         if (password_verify($password, $user->password)) {
             $this->user = $user;
+
+            // invalidate old session if exists
+            if ($this->user_session !== null) {
+                $this->user_session->logged_in = false;
+                $this->user_session->save();
+            }
+
+            // create a new session
             $this->user_session = new $this->user_session_cls(user_id: $user->id);
+            $this->user_session->logged_in = true; 
             $this->user_session->save();
             $this->is_authenticated = true;
 
@@ -116,6 +136,11 @@ class Auth
         return false;
     }
 
+    /**
+     * Logs a user out.  Invalidates the user session and deletes the auth token cookie.
+     *
+     * @return boolean Logout success.
+     */
     public function logout(): bool
     {
         $cookie_options = array (
@@ -129,10 +154,14 @@ class Auth
 
         setcookie($this->auth_token_name, "", $cookie_options);
 
+        //var_dump($this->user_session);
+
         if ($this->user_session !== null) {
             $this->user_session->logged_in = false;
             $this->user_session->save();
         }
+
+        //var_dump($this->user_session);
 
         return true;
     }
